cd41f059ff4ddde6a511aaf334a504a8d1bb6981
anyone <anyone@ao.com>
2026-01-07 10:27:08 +0800

    Revert "test2"
    This reverts commit f4935aa349add25eb2d4c671a50fd407f3629064.

--- src/main/java/com/caoccao/qjs4j/compiler/ast/SequenceExpression.java
+++ src/main/java/com/caoccao/qjs4j/compiler/ast/SequenceExpression.java
@@ -1,25 +1,12 @@
 /*
  * Copyright (c) 2025-2026. caoccao.com Sam Cao
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-
 package com.caoccao.qjs4j.compiler.ast;
 
 import java.util.List;
 
 /**
- * Represents a comma-separated sequence of expressions (the comma operator).
+ * Represents a comma-separated sequence expression (comma operator).
  */
 public record SequenceExpression(
         List<Expression> expressions,
